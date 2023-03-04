4a3cf0aed3fc343fc5a71b9a27cf386eaf63e817
Nica <alimurungnica14@gmail.com>
2023-03-04 11:06:28 +0800

    LoginSample

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, OnDestroy, ViewEncapsulation, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class LoginComponent implements OnInit, OnDestroy {
+
+  constructor(@Inject(DOCUMENT) private _document: any){}
+
+  ngOnInit() {
+    this._document.body.classList.add('body');
+  }
+
+  ngOnDestroy() {
+    this._document.body.classList.add('body');
+  }
+
+}
