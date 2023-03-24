59ec71f591aefeb6941830253a5b0533796f491c
Alipioallen <111748932+Alipioallen@users.noreply.github.com>
2023-03-24 13:36:58 +0800

    password strength

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { CheckEmailComponent } from './check-email/check-email.component';
 import { NewPassComponent } from './new-pass/new-pass.component';
 import { PassDoneComponent } from './pass-done/pass-done.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { StrengthMeterModule } from "ngx-strength-meter";
+import { MatInputModule } from "@angular/material/input";
+import { MatCardModule } from "@angular/material/card";
 
 @NgModule({
   declarations: [
@@ -18,12 +22,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     ForgotPassComponent,
     CheckEmailComponent,
     NewPassComponent,
-    PassDoneComponent
+    PassDoneComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatCardModule,
+    StrengthMeterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/new-pass/new-pass.component.ts
+++ src/app/new-pass/new-pass.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy, ViewEncapsulation, Inject } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-new-pass',
@@ -9,6 +10,11 @@ import { DOCUMENT } from '@angular/common';
   encapsulation: ViewEncapsulation.None,
 })
 export class NewPassComponent implements OnInit, OnDestroy {
+  form!: FormGroup;
+
+
+
+  
   visible:boolean = true;
   changetype:boolean = true;
   viewpass(){
@@ -21,14 +27,21 @@ export class NewPassComponent implements OnInit, OnDestroy {
     this.cvisible = !this.cvisible
     this.cchangetype = !this.cchangetype
   }
-  constructor(@Inject(DOCUMENT) private _document: any){}
+  constructor(@Inject(DOCUMENT) private _document: any, private fb: FormBuilder){}
 
-  ngOnInit() {
+  ngOnInit() : void{
     this._document.body.classList.add('body');
+    this.form = this.fb.group({
+      password: ['', Validators.required],
+    });
   }
 
   ngOnDestroy() {
     this._document.body.classList.add('body');
+  }
+
+  onStrengthChange(score: any) {
+    console.log('new score', score);
   }
 
 }
